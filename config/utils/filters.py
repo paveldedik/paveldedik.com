467a2920455d06ae93e78b69f5b3e78db1776db0
Pavel Dedik <dedikx@gmail.com>
2014-04-27 21:10:21 +0200

    Added jinja2 templates.

---
+++ config/utils/filters.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+
+import re
+from datetime import date
+from unicodedata import normalize
+
+
+_slug_regex = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+
+
+def copyright(year):
+    """Template filter that prints copyright.
+    """
+    current_year = date.today().year
+    if current_year == year:
+        period = str(year)
+    else:
+        period = '{0}&ndash;{1}'.format(year, current_year)
+    return u'&copy; {}'.format(period)
+
+
+def to_date(datum, format='%B %d, %Y'):
+    """Template filter that prints given date.
+    """
+    return datum.strftime(format)
+
+
+def slugify(text, delim=u'-', length=None):
+    """Generates an ASCII-only slug. A slug is the part of a URL which
+    identifies a page using human-readable keywords.
+    See `Generating Slugs<http://flask.pocoo.org/snippets/5/>`_.
+    """
+    result = []
+    text = text if length is None else text[:length]
+    for word in _slug_regex.split(text.lower()):
+        word = normalize('NFKD', word).encode('ascii', 'ignore')
+        if word:
+            result.append(word)
+    return unicode(delim.join(result))
